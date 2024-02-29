58ef02d97752e144e31ca802edee625a8360e1f9
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-02-29 09:36:36 +0300

    sayfalarda categoriye göre filtreleme sağlandı

--- src/Pages/Categories/Work.tsx
+++ src/Pages/Categories/Work.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { TaskListContext } from "../../Context/TaskListContext";
+import { HeaderTitle } from "../../Components/HeaderTitle";
+import { FilterBar } from "../../Components/FilterBar";
+import TaskCard from "../../Components/TaskCard";
+import Pagination from "../../Components/Pagination";
+import { TaskListType } from "../../Context/TaskType";
 
 export const WorkCategoriePage = () => {
+  const { filterTaskList } = useContext(TaskListContext) as TaskListType;
+
+  const homeTasks = filterTaskList.filter((task) => task.categorie === "Work");
+
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 4;
+
+  const totalPages = Math.ceil(homeTasks.length / itemsPerPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const displayedTasks = homeTasks.slice(startIndex, endIndex);
+
   return (
-    <div>ShoppingLis</div>
-  )
-}
+    <div className="flex flex-col gap-10">
+      <HeaderTitle title="All Types" />
+      <FilterBar />
+
+      <div className="flex flex-col gap-2">
+        {displayedTasks.map((task, index) => (
+          <TaskCard key={index} task={task} />
+        ))}
+      </div>
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
+};
