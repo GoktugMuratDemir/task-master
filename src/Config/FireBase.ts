fcb6d2e975c1418232e4c973c7bf9e7bab47d002
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-03-13 19:01:44 +0300

    .env firebase güncellendi

--- src/Config/FireBase.ts
+++ src/Config/FireBase.ts
@@ -12,22 +12,13 @@ interface FirebaseConfig {
   appId: string;
 }
 
-// const firebaseConfig: FirebaseConfig = {
-//   apiKey: `${process.env.REACT_APP_API_KEY }`|| "",
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN || "",
-//   projectId: process.env.REACT_APP_PROJECT_ID || "",
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET || "",
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || "",
-//   appId: process.env.REACT_APP_APP_ID || ""
-// };
-
 const firebaseConfig: FirebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.FIREBASE_APP_ID || "",
+  apiKey: `${process.env.REACT_APP_API_KEY }`|| "",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN || "",
+  projectId: process.env.REACT_APP_PROJECT_ID || "",
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || "",
+  appId: process.env.REACT_APP_APP_ID || ""
 };
 
 const app = initializeApp(firebaseConfig);
